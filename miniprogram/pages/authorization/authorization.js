227752d5687858636a8caabe624f65036faedea7
无心 <unknown@example.com>
2019-04-08 15:57:05 +0800

    修改数据库

--- miniprogram/pages/authorization/authorization.js
+++ miniprogram/pages/authorization/authorization.js
@@ -7,43 +7,19 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {},
+    hasUserInfo: false,
   },
 
- addData:function(event){
-console.log(event)
-nameCollection.add({
-  data:{
-    // "openId": "OPENID",
-    //  "nickName": "NICKNAME"
-    // userinfo-nickname
-    // "name":'getOpenid',
-    // "nickname": res.userInfo.nickName
-    // // "gender": GENDER,
-    // "city": "CITY",
-    // "province": "PROVINCE",
-    // "country": "COUNTRY",
-    // "avatarUrl": "AVATARURL",
-    // "unionId": "UNIONID"
-    // "avatarUrl": res.userInfo.avatarUrl,
-    // "userInfo": res.userInfo
-    // "watermark":
-    // {
-    //   "appid": "APPID",
-    //   "timestamp": TIMESTAMP
-    // }
 
-  }
-  })
-},
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var that = this;
-    var nickName = that.data.nickName;
-    var avatarUrl = that.data.avatarUrl;
-    var db = "no";
+
+
+
+
     // 获取用户信息
     wx.getSetting({
       success: res => {
@@ -51,14 +27,12 @@ nameCollection.add({
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
-              const userInfo = res.userInfo
-              const nickName = userInfo.nickName
-              const avatarUrl = userInfo.avatarUrl
-              const gender = userInfo.gender // 性别 0：未知、1：男、2：女
-              const province = userInfo.province
-              const city = userInfo.city
-              const country = userInfo.country
-             
+              console.log(res.userInfo)//调试：输出获取到的用户信息判断是否成功获取
+              this.setData({
+                userInfo: res.userInfo//把成功获取的内容存到这个page的data里面
+              })
+              console.log("success")//若完成上一步走到这一步的话输出“成功”
+              console.log(this.data.userInfo)//输出page的data里面userInfo这个列表，判断是否成功存进去了
             }
           })
         }
@@ -122,5 +96,29 @@ nameCollection.add({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+
+  addData: function (event) {
+    console.log(event)
+    nameCollection.add({
+      data: {
+        "nickName": this.data.userInfo.nickName,//读取这个页面的data里面userInfo这个列表的nickName项
+        // "gender": this.data.userInfo.gender,//同理
+        "avatarUrl" :this. data.userInfo.avatarUrl
+        // "city": "CITY",
+        // "province": "PROVINCE",
+        // "country": "COUNTRY",
+        // "avatarUrl": "AVATARURL",
+        // "unionId": "UNIONID"
+        // "avatarUrl": res.userInfo.avatarUrl,
+        // "userInfo": res.userInfo
+        // "watermark":
+        // {
+        //   "appid": "APPID",
+        //   "timestamp": TIMESTAMP
+        // }
+
+      }
+    })
+  },
 })
