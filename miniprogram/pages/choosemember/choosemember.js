a5496985a95c6f751269047214b53980192cec7c
无心 <unknown@example.com>
2019-04-08 16:11:12 +0800

    选择成员链接数据库

--- miniprogram/pages/choosemember/choosemember.js
+++ miniprogram/pages/choosemember/choosemember.js
@@ -1,5 +1,6 @@
 // pages/choosemember/choosemember.js
-
+const db =wx. cloud.database()
+const nameCollection = db.collection('name')
 Page({
 
   /**
@@ -13,7 +14,11 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+  nameCollection.get().then(res => {
+    this.setData({
+      name:res.data
+    })
+  })
   },
 
   /**
