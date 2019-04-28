a34138e3b4da30a81a90f0428eeb39cd5ffeec89
无心 <unknown@example.com>
2019-04-29 00:38:51 +0800

    +号修改

--- miniprogram/pages/task-list/task-list.js
+++ miniprogram/pages/task-list/task-list.js
@@ -8,24 +8,24 @@ Page({
    * 页面的初始数据
    */
   data: {
-    boxStyle:'box_b',
-    up:true,
-    down:false,
-    teamName:'',
-    memberNum:'',
-    teamIntroduce:'',
-    openId:'',
-    teamId:'',
-    userId:'',
-    taskList:[],
-    task:[],
-    userList:[],
-    uhide:0,
-    currentTab:'',
-    taskListLength:1,
+    boxStyle: 'box_b',
+    up: true,
+    down: false,
+    teamName: '',
+    memberNum: '',
+    teamIntroduce: '',
+    openId: '',
+    teamId: '',
+    userId: '',
+    taskList: [],
+    task: [],
+    userList: [],
+    uhide: 0,
+    currentTab: '',
+    taskListLength: 1,
     boxcolor: ["", "#fc2100", "#fff659", "#27e530", "#5a97f6"],
   },
-  bindMemberList: function () {
+  bindMemberList: function() {
     wx.redirectTo({
       url: '/pages/number-list/number-list',
     })
@@ -33,27 +33,27 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    console.log('【task-list】【index界面传入参数】',options)
+  onLoad: function(options) {
+    console.log('【task-list】【index界面传入参数】', options)
     this.setData({
-      teamId:options.teamId,
-      openId:options.openId,
-      userId:options.userId
+      teamId: options.teamId,
+      openId: options.openId,
+      userId: options.userId
     })
     var that = this;
     db.collection('team').where({
-      _id: options.teamId 
+      _id: options.teamId
     }).get({
       success(res) {
-       that.setData({
-          teamName:res.data[0].name,
-          memberNum:res.data[0].userNum,
-          teamIntroduce:res.data[0].introduce,
-          taskList:res.data[0].taskList,
-          userList:res.data[0].userList,
-          taskListLength:res.data[0].taskList.length*510
+        that.setData({
+          teamName: res.data[0].name,
+          memberNum: res.data[0].userNum,
+          teamIntroduce: res.data[0].introduce,
+          taskList: res.data[0].taskList,
+          userList: res.data[0].userList,
+          taskListLength: res.data[0].taskList.length * 510
         })
-        console.log('【task-list】【获取指定的team信息】【获取成功】',res.data[0])
+        console.log('【task-list】【获取指定的team信息】【获取成功】', res.data[0])
         for (var i = 0; i < that.data.taskList.length; i++) {
           db.collection('task').where({
             _id: that.data.taskList[i]
@@ -66,35 +66,35 @@ Page({
             }
           })
         }
-        console.log('【task-list】【获取指定的task信息】【获取成功】',that.data.task)
+        console.log('【task-list】【获取指定的task信息】【获取成功】', that.data.task)
       }
     })
- 
+
   },
   /*** 滑动切换tab***/
-  bindChange: function (e) {
+  bindChange: function(e) {
     var that = this;
     that.setData({
       currentTab: e.detail.current
     });
   },
   /*** 点击tab切换***/
-  swichNav: function (e) {
+  swichNav: function(e) {
     var that = this;
     that.setData({
       currentTab: e.target.dataset.current
     })
   },
-  longtapDelete:function(e){
+  longtapDelete: function(e) {
     var that = this;
     var task = that.data.task;
     var taskList = that.data.taskList;
-    var index = e.currentTarget.dataset.index;//获取当前长按图片下标
+    var index = e.currentTarget.dataset.index; //获取当前长按图片下标
     console.log(e)
     wx.showModal({
       title: '提示',
       content: '确定要删除此任务吗？',
-      success: function (res) {
+      success: function(res) {
         if (res.confirm) {
           console.log('【task-list】【长按删除】【点击确定】', '索引为：', index);
           task.splice(index, 1);
@@ -115,7 +115,7 @@ Page({
       }
     })
   },
-  onChangeShowState: function (event) {
+  onChangeShowState: function(event) {
     var that = this;
     // var toggleBtnVal = that.data.uhide;
     // var itemId = event.currenTarget.id;
@@ -123,17 +123,31 @@ Page({
 
     // }
   },
-  addTask:function(){
-    wx.navigateTo({
-      url: '/pages/create_task/create_task?teamId='+this.data.teamId+'&openId='+this.data.openId+'&userId='+this.data.userId+'&teamName='+this.data.teamName
-    })
+  addTask: function(res) {
+    var that = this;
+    if (this.data.currentTab) {
+        if (res.from === 'button') {
+          console.log('【invite】【分享方式】【通过button分享】', res)
+        }
+        return {
+          title: '邀请你加入新团队',
+          path: '/pages/beInvited/beInvited?teamId=' + that.data.teamId,
+          imageUrl: '/images/share.png',
+        }
+
+    } else {
+      wx.navigateTo({
+        url: '/pages/create_task/create_task?teamId=' + that.data.teamId + '&openId=' + that.data.openId + '&userId=' + that.data.userId + '&teamName=' + that.data.teamName
+      })
+    }
+
   },
-  bindTaskDetail:function(e){
+  bindTaskDetail: function(e) {
     var index = e.currentTarget.dataset.index;
     console.log(this.data.task[index]);
     var that = this;
     wx.navigateTo({
-      url: '/pages/task/task?taskId='+that.data.task[index]._id+'&teamId='+that.data.teamId
+      url: '/pages/task/task?taskId=' + that.data.task[index]._id + '&teamId=' + that.data.teamId
     })
   }
 })
