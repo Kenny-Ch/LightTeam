957a6f15eda91fb191fdd8d302fd9f44139980a4
无心 <unknown@example.com>
2019-05-11 19:39:46 +0800

    滑动

--- miniprogram/pages/authorization_main/authorization_main.js
+++ miniprogram/pages/authorization_main/authorization_main.js
@@ -5,62 +5,76 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    currentTab: '',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
-
+  /*** 滑动切换tab***/
+  bindChange: function(e) {
+    var that = this;
+    that.setData({
+      currentTab: e.detail.current
+    })
+  },
+  /*** 点击tab切换***/
+  swichNav: function(e) {
+    var that = this;
+    that.setData({
+      currentTab: e.target.dataset.current
+    })
+    // console.log(e.detail.setdata.current)
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
