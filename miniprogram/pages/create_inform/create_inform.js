84f1ad6d139de6b7be8b178645afdf29dad001be
明 <1445231750@qq.com>
2019-05-15 23:09:09 +0800

    js

--- miniprogram/pages/create_inform/create_inform.js
+++ miniprogram/pages/create_inform/create_inform.js
@@ -1,66 +1,163 @@
-// pages/create_inform/create_inform.js
+const db = wx.cloud.database()
+const taskCollection = db.collection('task')
 Page({
 
-  /**
-   * 页面的初始数据
+  /* 页面的初始数据
    */
   data: {
-
+    remind: 0,
+    multiIndex: [0, 0, 0],
+    taskid: '',
+    taskName: '',
+    taskIntroduction: '',
+    index: 0,
+    teamId: '',
+    userId: '',
+    taskList: [],
+    url: [],
+    batchIds: [],
+    accept: [],
+    unfinishTask: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
+    console.log('【create_form】【task-list界面传入参数】', options)
+    this.setData({
+      teamId: options.teamId,
+      openId: options.openId,
+      userId: options.userId,
+      teamName: options.teamName,
+      unfinishTask: parseInt(options.unfinishTask)
+    })
+    var that = this;
+    db.collection('user').where({
+      _id: that.data.userId
+    }).get({
+      success(res) {
+        // that.setData({
+        //   url0: res.data[0].avatarUrl
+        // })
+      }
+    })
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
-
+    var arr = new Array();
+    for (var i = 0; i < this.data.batchIds.length; i++) {
+      if (i <= 7) {
+        if (i == 7) {
+          arr.push('');
+        }
+        else {
+          arr.push(this.data.batchIds[i].Url);
+        }
+      }
+      else {
+        arr.push('');
+      }
+    }
+    // try {
+    //   wx.setStorageSync('batchIds', this.data.batchIds)
+    // } catch (e) {
+    // }
+    this.setData({
+      url: arr
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  bindTaskNameInput: function (e) {
+    this.setData({
+      taskName: e.detail.value
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  bindTaskIntroductionInput: function (e) {
+    this.setData({
+      taskIntroduction: e.detail.value
+    })
+},
+  bindSave: function (event) {
+    if (!this.data.taskName ||
+      !this.data.taskIntroduction ||
+      (this.data.batchIds.length == 0)) {
+      console.log('【create_form】【创建通知信息输入情况】【输入不完整】', event)
+      wx.showToast({
+        title: '通知的信息填写有误或不完整',
+        icon: 'none',
+        duration: 2000
+      })
+    } else {
+      var acceptarr = [];
+      var that = this;
+      for (var i = 0; i < this.data.batchIds.length; i++) {
+        console.log(i)
+        acceptarr.push(false);
+      }
+      this.setData({
+        accept: acceptarr
+      })
+      taskCollection.add({
+        data: {
+          "name": this.data.taskName,
+          "taskIntroduction": this.data.taskIntroduction,
+          "accept": this.data.accept,
+          "finish": false,
+          "userList": this.data.batchIds,
+          "team": this.data.teamId,
+          "teamName": this.data.teamName,
+          "type": 1
+        },
+        success: res => {
+          this.setData({
+            taskid: res._id,
+            unfinishTask: this.data.unfinishTask
+          })
+          // db.collection('user').doc(that.data.userId).update({
+          //   data: {
+          //     taskList: db.command.push(that.data.taskid)
+          //   },
+          // })
+          var ii = 0;
+          for (; ii < that.data.batchIds.length; ii++) {
+            wx.cloud.callFunction({
+              name: 'pushTaskId',
+              data: {
+                userId: that.data.batchIds[ii].id,
+                taskId: res._id
+              },
+            })
+          }
+          db.collection('team').doc(that.data.teamId).update({
+            data: {
+              taskList: db.command.push(that.data.taskid),
+              unfinishTask: that.data.unfinishTask + 1
+            },
+          })
+          let pages = getCurrentPages(); //获取当前页面js里面的pages里的所有信息。
+          let prevPage = pages[pages.length - 2]; //prevPage 是获取上一个页面的js里面的pages的所有信息。 -2 是上一个页面，-3是上上个页面以此类推。
+          db.collection('task').where({
+            _id: res._id
+          }).get({
+            success(res) {
+              prevPage.data.task.unshift(res.data[0])
+              prevPage.setData({
+                task: prevPage.data.task,
+                taskListLength: prevPage.data.taskListLength + 180,
+                unfinishTask: prevPage.data.unfinishTask + 1,
+                de: 0
+              })
+              //上一个页面内执行setData操作，将我们想要的信息保存住。当我们返回去的时候，页面已经处理完毕。
+              //最后就是返回上一个页面。
+              wx.navigateBack({
+                delta: 1 // 返回上一级页面。
+              })
+            }
+          })
+          console.log('【create_task】【添加任务信息】【成功添加任务信息】', res)
+        }
+      })
+    }
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  bindChooseMember: function () {
+    wx.navigateTo({
+      url: '/pages/choosemember/choosemember?teamId=' + this.data.teamId + '&userId=' + this.data.userId + '&openId=' + this.data.openId
+    })
   }
 })
--- miniprogram/pages/inform/inform.js
+++ miniprogram/pages/inform/inform.js
@@ -1,66 +1,143 @@
-// pages/inform/inform.js
+const db = wx.cloud.database()
+const _ = db.command
+const taskCollection = db.collection('task')
+const teamCollection = db.collection('team')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    teamName: '',
+    taskName: '',
+    taskIntroduction: '',
+    memberList: [],
+    finish: false,
+    tagNum: 0,
+    userList: [],
+    accept: [],
+    userId: '',
+    leaderId: '',
+    buttonHidden1: true,
+    buttonHidden2: true,
+    i: 0,
+    state: '',
+    taskId: '',
+    url: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      teamName: options.teamName,
+      taskId: options.taskId,
+      userId: options.userId,
+      leaderId: options.leaderId
+    })
+    console.log('【inform】【task-list界面传参】', options)
+    taskCollection.doc(options.taskId)
+      .get({
+        success: res => {
+          console.log('【inform】【获取指定用户task信息】【获取成功】', res.data),
+            this.setData({
+              taskName: res.data.name,
+              finish: res.data.finish,
+              memberList: res.data.userList,
+              accept: res.data.accept,
+              taskIntroduction: res.data.taskIntroduction
+            })
+          if (this.data.finish) {
+            this.setData({
+              state: "已完成"
+            })
+          } else {
+            this.setData({
+              state: currentDate < begin ? "未开始" : currentDate <= end ? "进行中" : "已截止"
+            })
+          }
+          for (var i = this.data.i; i < this.data.memberList.length; i++) {
+            if (this.data.userId == this.data.leaderId) {
+              this.setData({
+                buttonHidden1: true,
+                buttonHidden2: this.data.finish,
+              })
+              this.data.accept[this.data.i] = true;
+              var that = this;
+              db.collection('task').doc(that.data.taskId).update({
+                data: {
+                  accept: that.data.accept
+                }
+              })
+              break;
+            }
+            if (this.data.memberList[i].id == this.data.userId) {
+              this.setData({
+                buttonHidden1: this.data.accept[i],
+                buttonHidden2: this.data.finish,
+                i: i
+              })
+              if (!this.data.buttonHidden1) {
+                this.setData({
+                  buttonHidden2: true
+                })
+              }
+              break;
+            }
+          }
+          for (var i = 0; i < this.data.memberList.length; i++) {
+            if (i <= 7) {
+              if (i == 7) {
+                this.data.url.push();
+              }
+              else {
+                this.data.url.push(this.data.memberList[i].Url);
+              }
+            }
+            else {
+              this.data.url.push();
+            }
+          }
+          this.setData({
+            url: this.data.url
+          })
+        }
+      })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  bindMemberDetail: function () {
+    wx.navigateTo({
+      url: '/pages/memberList/memberList?accept=' + this.data.accept + '&taskId=' + this.data.taskId + '&finish=' + this.data.finish + '&teamName=' + this.data.teamName
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  bindReceiveTask: function () {
+    this.data.accept[this.data.i] = true;
+    var that = this;
+    wx.cloud.callFunction({
+      name: "updateAccept",
+      data: {
+        accept: that.data.accept,
+        taskId: that.data.taskId
+      },
+    }).then(res => {
+      that.setData({
+        accept: that.data.accept,
+        buttonHidden1: true,
+        buttonHidden2: false
+      })
+      console.log("【task】【已接受任务】【更新成功】")
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  finishtask: function () {
+    wx.cloud.callFunction({
+      name: 'finishTask',
+      data: {
+        taskId: this.data.taskId
+      },
+    })
+    this.setData({
+      buttonHidden2: true,
+      state: "已完成"
+    })
   }
 })
